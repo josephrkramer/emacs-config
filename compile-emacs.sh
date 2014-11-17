fc0b44127e42dc16040edf861f70732ad46ba4bf
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2014-11-17 12:51:52 -0500

    Add compile emacs script

---
+++ compile-emacs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+emacsVersion=emacs-24.3
+
+sudo apt-get install -y make libjpeg-dev libpng-dev libgif-dev libtiff-dev libncurses-dev
+wget -c http://ftp.gnu.org/gnu/emacs/$emacsVersion.tar.gz
+tar xfz $emacsVersion.tar.gz
+mv $emacsVersion.tar.gz $emacsVersion/.
+cd $emacsVersion
+./configure
+make
+sudo checkinstall
+cd ..
